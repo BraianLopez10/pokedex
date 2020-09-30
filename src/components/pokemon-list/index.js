f1111151284b1a718b986ab23ea3a6e8fd5b5f90
Braian Emanuel Lopez <braian.lopez.ze@outlook.com>
2020-09-30 07:19:44 -0300

    Refactor

--- src/components/pokemon-list/index.js
+++ src/components/pokemon-list/index.js
@@ -1,34 +1,39 @@
-import React, { useEffect } from 'react'
-import { chooseColor } from '../Utils/utils'
-import './index.scss'
+import React, { useEffect } from "react";
+import { chooseColor } from "../Utils/utils";
+import "./index.scss";
 export const PokemonList = ({ pokemon }) => {
+  useEffect(() => {}, []);
 
-    useEffect(() => {
-
-    }, [])
-
-
-    return (
-        <div className="wrap-pokemon-list " style={{ backgroundColor: chooseColor(pokemon.types[0].type.name).bg }}>
-            <a href={"/pokemon/" + pokemon.id}>
-                <div className="wrap-img">
-                    <img src={pokemon.sprites.front_default}></img>
-                </div>
-                <h4 className="name">{pokemon.name}</h4>
-                <div className="tipos">
-                    {pokemon.types.map((t, index) => {
-                        let color = chooseColor(t.type.name);
-                        return (
-                            <span key={index} style={{ backgroundColor: color.bg, color: color.font }}>
-                                <span> {t.type.name}</span>
-                            </span>
-                        )
-                    })}
-                </div>
-                <span className="span1">H <strong>{pokemon.height}</strong></span>
-                <span className="span2">W <strong>{pokemon.weight}</strong></span>
-            </a>
-        </div >
-
-    )
-}
+  return (
+    <div
+      className="wrap-pokemon-list "
+      style={{ backgroundColor: chooseColor(pokemon.types[0].type.name).bg }}
+    >
+      <a href={"/pokemon/" + pokemon.id}>
+        <div className="wrap-img">
+          <img src={pokemon.sprites.front_default} alt="pokemon-img"></img>
+        </div>
+        <h4 className="name">{pokemon.name}</h4>
+        <div className="tipos">
+          {pokemon.types.map((t, index) => {
+            let color = chooseColor(t.type.name);
+            return (
+              <span
+                key={index}
+                style={{ backgroundColor: color.bg, color: color.font }}
+              >
+                <span> {t.type.name}</span>
+              </span>
+            );
+          })}
+        </div>
+        <span className="span1">
+          H <strong>{pokemon.height}</strong>
+        </span>
+        <span className="span2">
+          W <strong>{pokemon.weight}</strong>
+        </span>
+      </a>
+    </div>
+  );
+};
